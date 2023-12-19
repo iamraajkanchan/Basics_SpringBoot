095e4f11d7ccbeea4006f0b1eb4fc8becb2a0d38
Raj Kanchan <iamraajkanchan@gmail.com>
2023-12-19 20:39:23 +0530

    Service class for Student is added in the project.

--- src/main/java/com/chinkyfamily/demo/Basics_SpringBoot/BasicsSpringBootApplication.java
+++ src/main/java/com/chinkyfamily/demo/Basics_SpringBoot/BasicsSpringBootApplication.java
@@ -2,19 +2,12 @@ package com.chinkyfamily.demo.Basics_SpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class BasicsSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BasicsSpringBootApplication.class, args);
     }
 
-    @GetMapping
-    public String homeRoute() {
-        return "Welcome to Spring Boot Basics";
-    }
 }
